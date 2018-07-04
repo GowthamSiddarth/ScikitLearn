68019fb055ca90a6bee1591a08152c7b3e751bde
Gowtham Behara <gothsiddu@gmail.com>
2018-07-04 21:49:03 +0530

    Use SVC to predict class from iris data

---
+++ IrisClassificationSVC.py
@@ -0,0 +1,11 @@
+from sklearn import datasets, svm
+
+iris = datasets.load_iris()
+X, y = iris.data[:-1], iris.target[:-1]
+
+clf = svm.SVC()
+clf.fit(X, y)
+
+prediction = clf.predict(iris.data[-1:])
+actual = iris.target[-1:]
+print("prediction = " + str(prediction) + ", actual = " + str(actual))
