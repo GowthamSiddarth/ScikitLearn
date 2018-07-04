b27bb4963142348277dff9fb0b3bba5f847333a3
Gowtham Behara <gothsiddu@gmail.com>
2018-07-04 21:48:36 +0530

    Compare model's prediction with actual target value

--- DigitsRecognitionSVC.py
+++ DigitsRecognitionSVC.py
@@ -6,5 +6,6 @@ X, y = digits.data[:-1], digits.target[:-1]
 clf = svm.SVC(gamma=0.1, C=100)
 clf.fit(X, y)
 
-res = clf.predict(digits.data[-1:])
-print(res)
+prediction = clf.predict(digits.data[-1:])
+actual = digits.target[-1:]
+print("prediction = " + str(prediction) + ", actual = " + str(actual))
