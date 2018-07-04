ebb186cf91ef041a13677768eabbc6234d85d730
Gowtham Behara <gothsiddu@gmail.com>
2018-07-04 22:40:15 +0530

    Use joblib from sklearn.externals to save clf

--- IrisClassificationSVC.py
+++ IrisClassificationSVC.py
@@ -1,4 +1,5 @@
 from sklearn import datasets, svm
+from sklearn.externals import joblib
 
 iris = datasets.load_iris()
 X, y = iris.data[:-1], iris.target[:-1]
@@ -9,3 +10,5 @@ clf.fit(X, y)
 prediction = clf.predict(iris.data[-1:])
 actual = iris.target[-1:]
 print("prediction = " + str(prediction) + ", actual = " + str(actual))
+
+joblib.dump(clf, "IrisClf.pkl")
