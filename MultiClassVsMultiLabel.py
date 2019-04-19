cb7aefff33310ed7fd6e2803c13de6e8bf875c08
Gowtham Behara <gothsiddu@gmail.com>
2019-04-19 17:33:42 +0530

    Use MultiLabelBinarizer and transform target data for multilabel classification

--- MultiClassVsMultiLabel.py
+++ MultiClassVsMultiLabel.py
@@ -1,6 +1,6 @@
 from sklearn.svm import SVC
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer
 
 X = [[1, 2], [2, 4], [4, 5], [3, 2], [3, 1]]
 y = [0, 0, 1, 1, 2]
@@ -10,5 +10,10 @@ clf.fit(X, y)
 print(clf.predict(X))
 
 y = LabelBinarizer().fit_transform(y)
+clf.fit(X, y)
+print(clf.predict(X))
+
+y = [[0, 1], [0, 2], [1, 3], [0, 2, 3], [2, 4]]
+y = MultiLabelBinarizer().fit_transform(y)
 clf.fit(X, y)
 print(clf.predict(X))
