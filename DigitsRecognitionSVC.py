7685f4e64d1fc0f575528d5c4e364a4f42903456
Gowtham Behara <gothsiddu@gmail.com>
2019-04-19 15:31:15 +0530

    Plot test sample and visualize

--- DigitsRecognitionSVC.py
+++ DigitsRecognitionSVC.py
@@ -1,4 +1,5 @@
 from sklearn import datasets, svm
+import matplotlib.pyplot as plt
 
 digits = datasets.load_digits()
 X, y = digits.data[:-1], digits.target[:-1]
@@ -9,3 +10,6 @@ clf.fit(X, y)
 prediction = clf.predict(digits.data[-1:])
 actual = digits.target[-1:]
 print("prediction = " + str(prediction) + ", actual = " + str(actual))
+
+plt.matshow(digits.images[-1])
+plt.show()
