fc11e8c048dc75a358ce18884131147d5e8115d2
Gowtham Behara <gothsiddu@gmail.com>
2019-04-19 16:37:58 +0530

    Auto typecasting to float64 is done for regression targets, not for classification targets

--- TypeCasting.py
+++ TypeCasting.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.random_projection import GaussianRandomProjection
+from sklearn import datasets, svm
 
 random_state = np.random.RandomState(0)
 X = random_state.randn(10, 10000)
@@ -12,3 +13,12 @@ transformer = GaussianRandomProjection()
 X_new = transformer.fit_transform(X)
 print(X_new.dtype)
 print(X.shape, X_new.shape)
+
+iris = datasets.load_iris()
+clf = svm.SVC(gamma='auto')
+
+clf.fit(iris.data, iris.target)
+print(clf.predict(iris.data[:3]))
+
+clf.fit(iris.data, iris.target_names[iris.target])
+print(clf.predict(iris.data[:3]))
