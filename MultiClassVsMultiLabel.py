bce1d899a64ffabe79822f4a07e0365b2aded600
Gowtham Behara <gothsiddu@gmail.com>
2019-04-19 17:30:15 +0530

    Change the format of target data and obtain prediction results depending on it

--- MultiClassVsMultiLabel.py
+++ MultiClassVsMultiLabel.py
@@ -1,9 +1,14 @@
 from sklearn.svm import SVC
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import LabelBinarizer
 
 X = [[1, 2], [2, 4], [4, 5], [3, 2], [3, 1]]
 y = [0, 0, 1, 1, 2]
 
 clf = OneVsRestClassifier(estimator=SVC(random_state=0, gamma='auto'))
+clf.fit(X, y)
+print(clf.predict(X))
+
+y = LabelBinarizer().fit_transform(y)
 clf.fit(X, y)
 print(clf.predict(X))
