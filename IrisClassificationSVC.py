de2a65b941dfee0754803f81a5f89951d056d7a0
Gowtham Behara <gothsiddu@gmail.com>
2019-04-21 14:43:59 +0530

    Use hyperparameters(kernel, C, gamme) in SVC and visualize different classification scenarios

--- IrisClassificationSVC.py
+++ IrisClassificationSVC.py
@@ -1,14 +1,27 @@
 from sklearn import datasets, svm
-from sklearn.externals import joblib
+import numpy as np
+import matplotlib.pyplot as plt
 
 iris = datasets.load_iris()
-X, y = iris.data[:-1], iris.target[:-1]
+# select first 2 features for visualizing in 2-D
+X, y = iris.data[:, :2], iris.target
 
-clf = svm.SVC()
+clf = svm.SVC(kernel='rbf', C=1000, gamma='auto')
 clf.fit(X, y)
 
-prediction = clf.predict(iris.data[-1:])
-actual = iris.target[-1:]
-print("prediction = " + str(prediction) + ", actual = " + str(actual))
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+h = (x_max / x_min) / 100
 
-joblib.dump(clf, "IrisClf.pkl")
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.subplot(1, 1, 1)
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.title('SVC with linear kernel')
+plt.show()
