6a0843d804eaea2cc06b3fe60c08ffa966d69116
Gowtham Behara <gothsiddu@gmail.com>
2019-04-21 16:15:30 +0530

    Use LinearRegression model to fit train data and print coefficients

--- DiabetesRegressionLinReg.py
+++ DiabetesRegressionLinReg.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
 import numpy as np
 
 diabetes = load_diabetes()
@@ -8,3 +9,8 @@ random_state = np.random.RandomState(0)
 indices = random_state.permutation(len(diabetes_X))
 diabetes_X_train, diabetes_y_train, diabetes_X_test, diabetes_y_test = \
     diabetes_X[indices[:-10]], diabetes_y[indices[:-10]], diabetes_X[indices[-10:]], diabetes_y[indices[-10:]]
+
+regr = LinearRegression()
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+print(regr.coef_)
