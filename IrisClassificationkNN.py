e4c6ca113aa0f32313c6a3589c064ba41cd13b8a
Gowtham Behara <gothsiddu@gmail.com>
2019-04-21 15:15:22 +0530

    Use kNN classifier to fit train data

--- IrisClassificationkNN.py
+++ IrisClassificationkNN.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
 iris = load_iris()
@@ -8,3 +9,5 @@ random_state = np.random.RandomState(0)
 indices = random_state.permutation(len(iris_X))
 iris_X_train, iris_y_train, iris_X_test, iris_y_test = iris_X[indices[:-10]], iris_y[indices[:-10]], \
                                                        iris_X[indices[-10:]], iris_y[indices[-10:]]
+clf = KNeighborsClassifier()
+clf.fit(iris_X_train, iris_y_train)
