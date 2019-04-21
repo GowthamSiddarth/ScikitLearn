048ec43ce9dbebb35525610ee2da9089844f0120
Gowtham Behara <gothsiddu@gmail.com>
2019-04-21 16:12:19 +0530

    Shuffle dataset and assign to train & test dataset

--- DiabetesRegressionLinReg.py
+++ DiabetesRegressionLinReg.py
@@ -1,4 +1,10 @@
 from sklearn.datasets import load_diabetes
+import numpy as np
 
 diabetes = load_diabetes()
 diabetes_X, diabetes_y = diabetes.data, diabetes.target
+
+random_state = np.random.RandomState(0)
+indices = random_state.permutation(len(diabetes_X))
+diabetes_X_train, diabetes_y_train, diabetes_X_test, diabetes_y_test = \
+    diabetes_X[indices[:-10]], diabetes_y[indices[:-10]], diabetes_X[indices[-10:]], diabetes_y[indices[-10:]]
