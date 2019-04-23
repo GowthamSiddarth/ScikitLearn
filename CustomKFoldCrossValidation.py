e901aee5b9feb9208f9d14509daaf593c9a3682a
Gowtham Behara <gothsiddu@gmail.com>
2019-04-23 23:01:56 +0530

    Split into 3 folds; pop test set and calc score from trained model foreach itr

--- CustomKFoldCrossValidation.py
+++ CustomKFoldCrossValidation.py
@@ -1,13 +1,17 @@
-from sklearn import datasets
+from sklearn import datasets, svm
 import numpy as np
 
 digits = datasets.load_digits()
 digits_X, digits_y = digits.data, digits.target
 
-random_state = np.random.RandomState(0)
-indices = random_state.permutation(len(digits_X))
+digits_X_fold, digits_y_fold = np.array_split(digits_X, 3), np.array_split(digits_y, 3)
+scores = []
 
-test_size = 0.1
-partition = int(test_size * len(digits_X))
-digits_X_train, digits_y_train, digits_X_test, digits_y_test = digits_X[indices[:-partition]], digits_y[
-    indices[:-partition]], digits_X[indices[-partition:]], digits_y[indices[-partition:]]
+for fold in range(3):
+    digits_X_train, digits_y_train = list(digits_X_fold), list(digits_y_fold)
+    digits_X_test, digits_y_test = digits_X_train.pop(fold), digits_y_train.pop(fold)
+    digits_X_train, digits_y_train = np.concatenate(digits_X_train), np.concatenate(digits_y_train)
+    clf = svm.SVC(kernel='linear').fit(digits_X_train, digits_y_train)
+    scores.append(clf.score(digits_X_test, digits_y_test))
+
+print(scores)
