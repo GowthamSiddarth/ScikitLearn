2062e0baabbc0fab1f469d29ab2f4af114d1033f
Gowtham Behara <gothsiddu@gmail.com>
2019-04-23 23:12:47 +0530

    Use cross_val_score and KFold from sklearn.model_selection

--- KFoldCrossValidation.py
+++ KFoldCrossValidation.py
@@ -1,4 +1,10 @@
 from sklearn.datasets import load_digits
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.svm import SVC
 
 digits = load_digits()
 digits_X, digits_y = digits.data, digits.target
+
+k_fold, clf = KFold(n_splits=3), SVC(kernel='linear')
+cross_val_scores = cross_val_score(estimator=clf, n_jobs=-1, X=digits_X, y=digits_y, cv=k_fold)
+print(cross_val_scores)
