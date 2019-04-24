db1e572c9a0cad90c3c4cab24ec0db4f95300d3e
Gowtham Behara <gothsiddu@gmail.com>
2019-04-24 09:09:05 +0530

    Create params_grid, use SVC for estimator and get best hyperparams for given train set

--- GridSearchCrossValidation.py
+++ GridSearchCrossValidation.py
@@ -1,4 +1,6 @@
 from sklearn.datasets import load_digits
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 import numpy as np
 
 digits = load_digits()
@@ -12,3 +14,15 @@ partition = int(test_size * len(digits_X))
 digits_X_train, digits_y_train, digits_X_test, digits_y_test = \
     digits_X[indices[:-partition]], digits_y[indices[:-partition]], \
     digits_X[indices[-partition:]], digits_y[indices[-partition:]]
+
+parameters_grid = [
+    {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+    {'C': [1, 10, 100, 1000], 'gamma': [0.1, 0.01, 0.001, 0.0001], 'kernel': ['rbf']}
+]
+
+clf = GridSearchCV(SVC(), param_grid=parameters_grid, cv=3, n_jobs=-1)
+clf.fit(digits_X_train, digits_y_train)
+
+print("Best score: ", clf.best_score_)
+print("Best params: ", clf.best_params_)
+print("Score on test set: ", clf.score(digits_X_test, digits_y_test))
